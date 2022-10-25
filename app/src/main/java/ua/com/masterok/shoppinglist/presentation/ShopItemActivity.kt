a313d9daf55efc7e16b6f488d8e01b8cc4c3b0db
SaienkoYaroslav <saienko.yaroslav01@gmail.com>
2022-10-25 19:41:54 +0300

    second screen model

---
+++ app/src/main/java/ua/com/masterok/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package ua.com.masterok.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ua.com.masterok.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
