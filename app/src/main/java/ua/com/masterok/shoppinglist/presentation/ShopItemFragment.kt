f7021c124d57737a5d4783c0f908830780206171
SaienkoYaroslav <saienko.yaroslav01@gmail.com>
2022-10-28 20:34:36 +0300

    fragment first step

---
+++ app/src/main/java/ua/com/masterok/shoppinglist/presentation/ShopItemFragment.kt
@@ -0,0 +1,18 @@
+package ua.com.masterok.shoppinglist.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ua.com.masterok.shoppinglist.R
+
+class ShopItemFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_shop_item, container, false)
+    }
+}
